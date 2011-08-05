629bb98c752e084b164d984ca0820534cd29f5d7
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-08-05 13:51:19 +0300

    removed extra files

--- opensocial_people/templates/javascript/softgis.esri.js
+++
@@ -1,8 +0,0 @@
-/* global dojo, djConfig, console, esri  */
-{% extends "javascript/softgis_client.commons.js" %}
-
-{% block library_specific_block %}
-	{% for client in softgis_clients %}
-	{{ client }}
-	{% endfor %}
-{% endblock %}
