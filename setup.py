2308d1c7dcd40cae0e62d0aeac956da93221e154
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-08-25 12:04:37 +0300

    added setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='opensocial_people',
+    version='1.0.0',
+    author='Kristoffer Snabb',
+    url='https://github.com/geonition/django_opensocial_people',
+    packages=['opensocial_people'],
+)
