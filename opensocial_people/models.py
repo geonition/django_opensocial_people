422bd84d54a1e0f44489ba1624d2d0c170573585
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-09-15 15:08:15 +0300

    signal reciever added to create a person object when a user object has been created, other small improvement done like __unicode__ functions

--- opensocial_people/models.py
+++ opensocial_people/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.db import IntegrityError
+from django.utils import simplejson as json
 from geonition_utils.manager import MongoDBManager
 from geonition_utils.models import JSON
 from geonition_utils.models import TimeD
@@ -12,6 +13,7 @@ from django.conf import settings
 import django
 from datetime import datetime
 import sys
+from django.db.models.signals import post_save
 
 class Relationship(models.Model):
     """ The Relationship model describes a link between two users """
@@ -20,6 +22,12 @@ class Relationship(models.Model):
     group = models.CharField(max_length = 10)
     target_user = models.ForeignKey(User, related_name='target_user')
     
+    
+    def __unicode__(self):
+        return u"%s -> %s -> %s" % (self.initial_user.username,
+                                    self.group,
+                                    self.target_user.username)
+        
     class Meta:
         unique_together = ('initial_user',
                            'group',
@@ -36,8 +44,8 @@ class Person(models.Model):
     """
     user = models.ForeignKey(User)
     json_data = models.ForeignKey(JSON)
-    time = models.ForeignKey(TimeD)
-            
+    time = models.ForeignKey(TimeD)        
+        
     def update(self, json_string, *args, **kwargs):
         """
         Updates this persons information.
@@ -65,10 +73,41 @@ class Person(models.Model):
         
     def delete(self, *args, **kwargs):
         self.time.expire()
+     
+    def __unicode__(self):
+        return u'for user %s' % (self.user.username,)
         
     class Meta:
         unique_together = (("time", "json_data", "user"),)
         permissions = (
             ("data_view", "Can view other's data"),
         )
+
+# a signal reciever is added to create e person after a django user
+# has been created
+def create_person(sender, instance, created, **kwargs):
+    if created:
+        default_person = {
+            "id": instance.id,
+            "username": instance.username,
+            "first_name": instance.first_name,
+            "last_name": instance.last_name,
+            "email": {
+                "value": instance.email,
+                "type": "",
+                "primary": True
+            }
+        }
+        json_obj = JSON(collection='opensocial_people_person',
+                        json_string=json.dumps(default_person))
+        json_obj.save()
+        time_obj = TimeD()
+        time_obj.save()
         
+        Person(user=instance,
+               json_data=json_obj,
+               time=time_obj).save()
+
+post_save.connect(create_person,
+                  sender=User,
+                  dispatch_uid="opensocial_people.person")
