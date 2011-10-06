f6db061f5ac34471d1ac10b5578345a8758bdcd7
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-10-06 15:41:19 +0300

    setup script modified

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
-from distutils.core import setup
+from setuptools import setup
+from setuptools import find_packages
 
 setup(
     name='opensocial_people',
-    version='1.0.0',
+    version='1.0.1',
     author='Kristoffer Snabb',
     url='https://github.com/geonition/django_opensocial_people',
-    packages=['opensocial_people'],
+    packages=find_packages(),
+    include_package_data=True,
+    package_data = {
+        "opensocial_people": [
+            "templates/*.js"
+        ],
+    },
+    zip_safe=False,
+    install_requires=['django']
 )
