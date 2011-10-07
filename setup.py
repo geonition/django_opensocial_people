a130aae52c575c0c4d30c11ba213c8bc8b0e5dd9
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-10-07 13:30:33 +0300

    setup version changed

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='opensocial_people',
-    version='1.0.1',
+    version='1.0.2',
     author='Kristoffer Snabb',
     url='https://github.com/geonition/django_opensocial_people',
     packages=find_packages(),
