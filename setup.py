570df26547818a2c86ce4d0de18364151b14e6d7
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-11-11 16:31:20 +0200

    version increased by one

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='opensocial_people',
-    version='1.1.1',
+    version='1.1.2',
     author='Kristoffer Snabb',
     url='https://github.com/geonition/django_opensocial_people',
     packages=find_packages(),
